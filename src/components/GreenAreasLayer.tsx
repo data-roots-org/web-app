d26b556562afbeb16dca0235c943721f805dee5d
Antonio <toninetocontato@gmail.com>
2025-10-05 22:44:21 -0300

    update green layer style

--- src/components/GreenAreasLayer.tsx
+++ src/components/GreenAreasLayer.tsx
@@ -1,35 +1,31 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { GeoJSON, useMap } from 'react-leaflet';
-import { useEffect, useState } from 'react';
-import type { FeatureCollection } from 'geojson';
-import L from 'leaflet';
+import { GeoJSON, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
+import type { FeatureCollection } from "geojson";
+import L from "leaflet";
 
 const GreenAreasLayer = () => {
-  const [geoJsonData, setGeoJsonData] = useState<FeatureCollection | null>(null);
+  const [geoJsonData, setGeoJsonData] = useState<FeatureCollection | null>(
+    null
+  );
   const map = useMap();
 
   useEffect(() => {
-    fetch('/ribeirao-preto.geojson')
+    fetch("/ribeirao-preto.geojson")
       .then((response) => response.json())
       .then((data) => {
         setGeoJsonData(data);
-        
-        const bounds = L.geoJSON(data).getBounds();
-        if (bounds.isValid()) {
-          map.fitBounds(bounds);
-        }
-
       })
       .catch((error) => console.error("Erro ao carregar o GeoJSON:", error));
   }, [map]);
 
   const styleGeoJSON = () => {
     return {
-      fillColor: '#28a745',
+      fillColor: "#28a745",
       weight: 1,
       opacity: 1,
-      color: '#006400',
-      fillOpacity: 0.5,
+      color: "#006400",
+      fillOpacity: 0.2,
     };
   };
 
@@ -40,16 +36,19 @@ const GreenAreasLayer = () => {
   };
 
   const filterPolygons = (feature: any) => {
-    return feature.geometry.type === 'Polygon' || feature.geometry.type === 'MultiPolygon';
+    return (
+      feature.geometry.type === "Polygon" ||
+      feature.geometry.type === "MultiPolygon"
+    );
   };
-  
+
   if (!geoJsonData) {
     return null;
   }
 
   return (
-    <GeoJSON 
-      data={geoJsonData} 
+    <GeoJSON
+      data={geoJsonData}
       style={styleGeoJSON}
       onEachFeature={onEachFeature}
       filter={filterPolygons}
